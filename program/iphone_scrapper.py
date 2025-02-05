24180537fd435ce644379b0ec9acb12f0e06f164
jogarman <122149837+jogarman@users.noreply.github.com>
2025-02-06 00:47:29 +0100

    creada carpeta programs y bugs resueltos

--- program/iphone_scrapper.py
+++ program/iphone_scrapper.py
@@ -1,59 +1,61 @@
 # Este código solo ejecuta reiteradas veces 01_wallascrap.py con los distintos parametros 
 # para IPHONE 14 15 y 16
-import sys
-import subprocess
-import os
 
-def run_wallascrap(item_name, municipio, estado, distancia, precio_minimo):
-    print("def run_wallascrap...")
-    python_executable = os.path.join('.env', 'Scripts', 'python.exe')
-    command = [
-        python_executable, '01_wallascrap.py',
-        '--item_name', item_name,
-        '--municipio', municipio,
-        '--estado', estado,
-        '--distancia', str(distancia),
-        '--precio_minimo', str(precio_minimo)
-    ]
-    print("command: ", command)
-    result = subprocess.run(command, capture_output=True, text=True)
-    # Mostrar la salida en el widget de texto
-    print(result.stdout)
-    if result.stderr:
-        print(result.stderr)
-
+from utils import convertir_ipynb_en_py, ejecutar_py, run_wallascrap, borrar_temp
 
 # 'new', 'as_good_as_new', 'good'
+def run_01_scrapper():
+    item_name = 'iphone 14'
+    municipio = 'Madrid'
+    estado = 'new'
+    distancia = '60'
+    precio_minimo = '250'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'as_good_as_new'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'good'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
 
-item_name = 'iphone 14'
-municipio = 'Madrid'
-estado = 'new'
-distancia = '60'
-precio_minimo = '250'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'as_good_as_new'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'good'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    item_name = 'iphone 15'
+    municipio = 'Madrid'
+    estado = 'new'
+    distancia = '60'
+    precio_minimo = '300'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'as_good_as_new'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'good'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
 
-item_name = 'iphone 15'
-municipio = 'Madrid'
-estado = 'new'
-distancia = '60'
-precio_minimo = '300'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'as_good_as_new'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'good'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    item_name = 'iphone 16'
+    municipio = 'Madrid'
+    estado = 'new'
+    distancia = '60'
+    precio_minimo = '300'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'as_good_as_new'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+    estado = 'good'
+    run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
 
-item_name = 'iphone 16'
-municipio = 'Madrid'
-estado = 'new'
-distancia = '60'
-precio_minimo = '300'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'as_good_as_new'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
-estado = 'good'
-run_wallascrap(item_name, municipio, estado, distancia, precio_minimo)
+######################################
+#####        02_join_tables      #####
+######################################
+
+
+def run(program_ipynb_ini):
+    print("running: ", program_ipynb_ini)
+    program_ipynb_temp = program_ipynb_ini.replace(".ipynb", "_temp.py")
+    convertir_ipynb_en_py(program_ipynb_ini)
+    ejecutar_py(program_ipynb_temp)
+    borrar_temp(program_ipynb_temp)
+
+
+run_01_scrapper()
+run("02_join_tables_iphone.ipynb")
+run("03_feature_engineering_iphone.ipynb")
+run("04_download_description.ipynb")
+run("04_download_description.ipynb")
+run("05_get_data_from_comments.ipynb")
+
+
